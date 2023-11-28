f983059446ae196584a2a7d1dc540ce3e3c45440
seveent <144870441+seveent@users.noreply.github.com>
2023-11-28 10:08:29 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const userrouter = require('./router/siswaa')
+const connectDB = require('./config/db')
+const port = 3000
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+connectDB()
+app.use(userrouter)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
